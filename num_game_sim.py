512ed59c0f3bc129192e9054c2aa320546ae2c50
ktgw <q863a@yahoo.co.jp>
2023-07-14 15:50:10 +0900

    Revert "単純なアルゴリズム"
    This reverts commit 2307760324771f1ece370f2a437c6c48b121ad71.

--- num_game_sim.py
+++ num_game_sim.py
@@ -10,6 +10,24 @@ import random
 
 max_num = 20000
 
+def calc_optimum_A(min_possible_answer: int):
+  """
+  最適なAを計算する関数
+  min_possible_answer (int): 正解候補の最小値(前回の解答した値 + 1)
+  """
+  max_val = None
+  A = min_possible_answer
+  for i in range(min_possible_answer, max_num):
+    val = ((200002 - i - min_possible_answer) - 100 * (i - min_possible_answer + 1) * (i - min_possible_answer)) / (20001 - min_possible_answer)
+    if max_val is None or val > A:
+      max_val = val
+      A = i
+    else:
+      break
+
+  return A
+
+
 def gen_A(correct: int, pre_ans: int) -> int:
   """
   correct (int): 前の回答の正誤．0:セーフ，1:正解，2:アウト
@@ -21,26 +39,52 @@ def gen_A(correct: int, pre_ans: int) -> int:
     '''
     最初の回答
     '''
-    ans = 1
+    ans = calc_optimum_A(1)
 
   elif correct == 0:
     '''
     前の回答がセーフの場合
     '''
-    ans = pre_ans + 1
+    ans = calc_optimum_A(pre_ans + 1)
 
   elif correct == 1:
     '''
     前の回答が正解の場合
     '''
-    ans = 1
+    ans = calc_optimum_A(1)
 
   elif correct == 2:
     '''
     前の回答がアウトの場合
     '''
-    ans = 1
+    ans = calc_optimum_A(1)
   
+  
+  """
+  if pre_ans == 0: 
+    '''
+    最初の回答
+    '''
+    ans = calc_optimum_A(1)
+
+  elif correct == 0:
+    '''
+    前の回答がセーフの場合
+    '''
+    ans = calc_optimum_A(pre_ans + 1)
+
+  elif correct == 1:
+    '''
+    前の回答が正解の場合
+    '''
+    ans = calc_optimum_A(1)
+
+  elif correct == 2:
+    '''
+    前の回答がアウトの場合
+    '''
+    ans = calc_optimum_A(1)
+  """
   print(ans, end = ',')
 
   return ans
@@ -91,11 +135,10 @@ def game(count_game: int):
   return score
 
 sum_score = 0
-num_sim = 100 # ゲームのシミュレーション回数
+num_sim = 1
 for i in range(num_sim):
   sum_score += game(i)
 ave_score = sum_score / num_sim
-
 print(f'{ave_score=}')
 
         
